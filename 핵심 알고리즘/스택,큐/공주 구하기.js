72b741d821704a2f3a63b6102851bd36a55e594e
intae kim <intaemon@intaeui-MacBookAir.local>
2021-12-07 15:42:57 +0900

    fix: 공주구하기 문제 큐로 풀어봄

--- 핵심 알고리즘/스택,큐/공주 구하기.js
+++ 핵심 알고리즘/스택,큐/공주 구하기.js
@@ -1,13 +1,11 @@
-// TODO:  큐 자료구조로 다시 풀어보기
 function solution(n, k) {
-  let arr = new Array(n).fill(0).map((_, i) => i + 1);
-  while (arr.length > 1) {
-    const targetIndex = k % arr.length === 0 ? k - 1 : (k % arr.length) - 1;
-    const left = arr.slice(0, targetIndex);
-    const right = arr.slice(targetIndex + 1);
-    arr = [...right, ...left];
+  let queue = new Array(n).fill(0).map((_, i) => i + 1);
+  while (queue.length > 1) {
+    for (let i = 0; i < k - 1; i++) {
+      queue.push(queue.shift());
+    }
+    queue.shift();
   }
-
-  return arr[0];
+  return queue.shift();
 }
 console.log(solution(8, 3));
